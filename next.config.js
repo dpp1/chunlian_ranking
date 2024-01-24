714215d1c600c66936fdfe76b5852ef9b4376730
Eric Sun <sunxiang@amazon.com>
2024-01-24 16:47:51 +0800

    Deprecate webpack that could cause errors

--- next.config.js
+++ next.config.js
@@ -5,21 +5,6 @@ const nextConfig = {
   },
   output: 'export',
   reactStrictMode: false,
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.(mp3)$/,
-            use: {
-              loader: 'file-loader',
-              options: {
-                publicPath: '/_next/static/sounds/',
-                outputPath: 'static/sounds/',
-                name: '[name].[ext]',
-                esModule: false,
-              },
-            },
-          });
-      return config;
-    },
   transpilePackages: [
       '@douyinfe/semi-ui',
     '@douyinfe/semi-icons',
