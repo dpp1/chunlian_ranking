b72b643d786ed4d645d1fe2c952668c30d139852
Eric Sun <sunxiang@amazon.com>
2024-01-26 00:37:27 +0800

    [Backend] Fanout requests to backup accounts

--- amplify/backend/function/chunlianMasterLambda/src/app.js
+++ amplify/backend/function/chunlianMasterLambda/src/app.js
@@ -8,34 +8,88 @@ See the License for the specific language governing permissions and limitations 
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-const { TextDecoder } = require('util');
-const { BedrockRuntimeClient, InvokeModelCommand } = require("@aws-sdk/client-bedrock-runtime");
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const {TextDecoder} = require('util');
+const {BedrockRuntimeClient, InvokeModelCommand} = require(
+    '@aws-sdk/client-bedrock-runtime');
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require(
+    'aws-serverless-express/middleware');
 const fs = require('fs');
 const path = require('path');
+const {defaultProvider} = require('@aws-sdk/credential-provider-node');
 
 // declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
 });
 
 // Load the content of the prompt files at startup
 const prompts = {
-  SET_THEME: fs.readFileSync(path.join(__dirname, 'initial-prompt-set-theme.txt'), 'utf8'),
-  CHUNLIAN_GEN: fs.readFileSync(path.join(__dirname, 'initial-prompt-chunlian-gen.txt'), 'utf8'),
-  CHUNLIAN_REVIEW: fs.readFileSync(path.join(__dirname, 'initial-prompt-chunlian-review.txt'), 'utf8'),
+  SET_THEME: fs.readFileSync(
+      path.join(__dirname, 'initial-prompt-set-theme.txt'), 'utf8'),
+  CHUNLIAN_GEN: fs.readFileSync(
+      path.join(__dirname, 'initial-prompt-chunlian-gen.txt'), 'utf8'),
+  CHUNLIAN_REVIEW: fs.readFileSync(
+      path.join(__dirname, 'initial-prompt-chunlian-review.txt'), 'utf8'),
 };
 
-const bedrockRuntimeClient = new BedrockRuntimeClient();
+// Primary Client used for All-hands booth
+const reservedClient = new BedrockRuntimeClient();
+
+// Define the role ARNs for your backup accounts
+const roleArns = [
+  "arn:aws:iam::242129332773:role/BedrockInvokeRole",
+  "arn:aws:iam::203443299887:role/BedrockInvokeRole",
+  "arn:aws:iam::276545714784:role/BedrockInvokeRole",
+  "arn:aws:iam::480709606956:role/BedrockInvokeRole",
+  "arn:aws:iam::913758768215:role/BedrockInvokeRole",
+  "arn:aws:iam::915526176907:role/BedrockInvokeRole"
+];
+
+// Function to assume a role and return credentials
+async function assumeRole(roleArn) {
+  const stsClient = new STSClient({region: 'your-region'});
+  const assumeRoleCommand = new AssumeRoleCommand({
+    RoleArn: roleArn,
+    RoleSessionName: 'bedrock-session-' +
+        Math.random().toString(36).substring(7), // Generate a unique session name
+  });
+
+  const response = await stsClient.send(assumeRoleCommand);
+  return {
+    accessKeyId: response.Credentials.AccessKeyId,
+    secretAccessKey: response.Credentials.SecretAccessKey,
+    sessionToken: response.Credentials.SessionToken,
+  };
+}
+
+// Initialize a pool of Bedrock clients
+const bedrockClients = roleArns.map((roleArn) => {
+  const credentials = defaultProvider({
+    roleAssumer: async (sourceCreds, params) => {
+      return assumeRole(roleArn);
+    },
+  });
+
+  const bedrockClient = new BedrockRuntimeClient({
+    credentials: credentials,
+    region: 'us-west-2',
+  });
+
+  return {
+    client: bedrockClient,
+    identifier: roleArn
+  };
+});
+
 const modelId = 'anthropic.claude-instant-v1';
 /* The different model providers have individual request and response formats.
    * For the format, ranges, and default values for Anthropic Claude, refer to:
@@ -46,7 +100,7 @@ const modelParameters = {
   temperature: 0.7,
   top_p: 0.54,
   top_k: 398,
-  stop_sequences: [ '\n\nHuman:' ],
+  stop_sequences: ['\n\nHuman:'],
 };
 
 /**
@@ -55,44 +109,49 @@ const modelParameters = {
  *
  * @param current_step
  * @param {string} prompt - The prompt that you want Claude to complete.
+ * @param isFromBooth
+ * @param bedrockClient
  * @returns {string} The inference response (completion) from the model.
  */
-const invokeClaude = async (current_step, prompt) => {
-  // Use the loaded prompt from the prompts object
-  const initialPrompt = prompts[current_step];
+const invokeClaude = async (current_step, prompt,
+        isFromBooth, bedrockClient) => {
 
-  if (!initialPrompt) {
-    console.log("Invalid step:", current_step);
-    return "Invalid step";
-  }
+      // Use the loaded prompt from the prompts object
+      const initialPrompt = prompts[current_step];
 
-  /* Claude requires you to enclose the prompt as follows: */
-  const enclosedPrompt = `Human: ${initialPrompt}\\n\\n${prompt}\n\nAssistant:`;
-  const payload = {
-    ...modelParameters,
-    prompt: enclosedPrompt
-  }
+      if (!initialPrompt) {
+        console.log('Invalid step:', current_step);
+        return 'Invalid step';
+      }
 
-  const command = new InvokeModelCommand({
-    body: JSON.stringify(payload),
-    contentType: 'application/json',
-    accept: 'application/json',
-    modelId,
-  });
+      /* Claude requires you to enclose the prompt as follows: */
+      const enclosedPrompt = `Human: ${initialPrompt}\\n\\n${prompt}\n\nAssistant:`;
+      const payload = {
+        ...modelParameters,
+        prompt: enclosedPrompt,
+      };
 
-  try {
-    const response = await bedrockRuntimeClient.send(command);
-    const decodedResponseBody = new TextDecoder().decode(response.body);
+      const command = new InvokeModelCommand({
+        body: JSON.stringify(payload),
+        contentType: 'application/json',
+        accept: 'application/json',
+        modelId,
+      });
 
-    /** @type {ResponseBody} */
-    const responseBody = JSON.parse(decodedResponseBody);
+      try {
+        const response = await bedrockClient.send(command);
+        const decodedResponseBody = new TextDecoder().decode(response.body);
 
-    return responseBody.completion;
+        /** @type {ResponseBody} */
+        const responseBody = JSON.parse(decodedResponseBody);
 
-  } catch (err) {
-    console.error(err);
-  }
-};
+        return responseBody.completion;
+
+      } catch (err) {
+        console.error(err);
+      }
+    }
+;
 
 /**********************
  * Example get method *
@@ -111,55 +170,50 @@ app.get('/chunlian-master/*', function(req, res) {
 /****************************
  * POST Method to for Chunlian Master actions*
  ****************************/
-
-/**
- * Tries to find and parse the first JSON object in a string.
- *
- * @param {string} text - The string to parse.
- * @returns {{parsedJson: Object|null, isValid: boolean}} An object containing the parsed JSON and a validity flag.
- */
-function parseFirstJson(text) {
-  try {
-    // Regex to match the first JSON object
-    const jsonMatch = text.match(/{.*?}/);
-    if (jsonMatch && jsonMatch[0]) {
-      return {
-        parsedJson: JSON.parse(jsonMatch[0]),
-        isValid: true
-      };
-    }
-    return { parsedJson: null, isValid: false };
-  } catch (error) {
-    console.error("Error parsing JSON:", error);
-    return { parsedJson: null, isValid: false };
-  }
-}
-
 app.post('/chunlian-master', async function(req, res) {
   //TODO: edge cases
-  console.log(req.body);
+
+  console.log('Request body: ', req.body);
   const inputPrompt = req.body.prompt;
-  const current_step = req.body.current_step;
+  const currentStep = req.body.current_step;
+  const isFromBooth = !!req.body.is_from_booth;
+  let bedrockClient;
+  if (isFromBooth) {
+    console.log('isFromBooth is true, using reserved client');
+    bedrockClient = reservedClient;
+  } else {
+    console.log('isFromBooth is false, using backup client');
+    const randomIndex = Math.floor(Math.random() * bedrockClients.length);
+    const selectedClientInfo = bedrockClients[randomIndex];
+    bedrockClient = selectedClientInfo.client
+    console.log(`Using Bedrock client with identifier: ${selectedClientInfo.identifier}`);
+  }
+
   let status;
   let result;
 
-  if (current_step === 'SET_THEME') {
-    const completionOfSetTheme = await invokeClaude(current_step, inputPrompt);
+  if (currentStep === 'SET_THEME') {
+    const completionOfSetTheme = await invokeClaude(currentStep, inputPrompt,
+        isFromBooth, bedrockClient);
     console.log('Completion of SET_THEME:', completionOfSetTheme);
-    const { parsedJson, isValid } = parseFirstJson(completionOfSetTheme);
 
-    const outputOfSetTheme = JSON.parse(completionOfSetTheme);
-    if (!isValid || !parsedJson || !parsedJson.theme) {
-      status = 'ERROR';
-      console.error('Invalid or missing theme in output of SET_THEME', completionOfSetTheme);
-    } else {
-      const completionOfChunlianGen = await invokeClaude('CHUNLIAN_GEN', outputOfSetTheme.theme);
-      console.log('Completion of CHUNLIAN_GEN:', completionOfChunlianGen);
-      status = 'OK';
-      result = completionOfChunlianGen;
-    }
-  } else if (current_step === 'CHUNLIAN_REVIEW') {
-    const completionOfChunlianReview = await invokeClaude('CHUNLIAN_REVIEW', inputPrompt);
+    let outputOfSetTheme;
+    outputOfSetTheme = extractJson(completionOfSetTheme);
+
+    const promptForChunlianGen = (outputOfSetTheme === undefined
+        || outputOfSetTheme.theme === undefined)
+        ? inputPrompt // Use customer input as theme as a backup
+        : outputOfSetTheme.theme;
+
+    const completionOfChunlianGen = await invokeClaude('CHUNLIAN_GEN',
+        promptForChunlianGen, isFromBooth, bedrockClient);
+    console.log('Completion of CHUNLIAN_GEN:', completionOfChunlianGen);
+    status = 'OK';
+    result = completionOfChunlianGen;
+
+  } else if (currentStep === 'CHUNLIAN_REVIEW') {
+    const completionOfChunlianReview = await invokeClaude('CHUNLIAN_REVIEW',
+        inputPrompt, isFromBooth, bedrockClient);
     console.log('Completion of CHUNLIAN_REVIEW:', completionOfChunlianReview);
     status = 'OK';
     result = completionOfChunlianReview;
@@ -168,9 +222,30 @@ app.post('/chunlian-master', async function(req, res) {
   res.json({result: result, status: status});
 });
 
+function extractJson(data) {
+  // fetch json out incase output is not just a json
+  const leftBracketIndex = data.indexOf('{');
+  const rightBracketIndex = data.lastIndexOf('}');
+  let parseResult;
+  if (leftBracketIndex >= 0 && rightBracketIndex >= 0 && leftBracketIndex <
+      rightBracketIndex) {
+    try {
+      parseResult = JSON.parse(
+          data.substring(leftBracketIndex, rightBracketIndex + 1),
+      );
+    } catch (e) {
+      console.error('Can\'t parse JSON from: ', data);
+    }
+  } else {
+    console.error('Can\'t parse JSON from: ', data);
+  }
+
+  return parseResult;
+}
+
 app.post('/chunlian-master/*', function(req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({success: 'post call succeed!', url: req.url, body: req.body});
 });
 
 /****************************
@@ -179,12 +254,12 @@ app.post('/chunlian-master/*', function(req, res) {
 
 app.put('/chunlian-master', function(req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({success: 'put call succeed!', url: req.url, body: req.body});
 });
 
 app.put('/chunlian-master/*', function(req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  res.json({success: 'put call succeed!', url: req.url, body: req.body});
 });
 
 /****************************
@@ -202,10 +277,10 @@ app.delete('/chunlian-master/*', function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log("App started")
+  console.log('App started');
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
